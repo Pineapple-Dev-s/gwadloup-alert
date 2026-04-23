fb3ef21e4dea5ce41904b497cef894cd5922ac83
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-04-23 01:52:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const compression = require('compression');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+const { marked } = require('marked');
+const html = marked(md);
+res.json({ html });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
